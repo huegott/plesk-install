4b0f6596cbacbf55c6c61e3bb12ca0a052f5b6dc
Huegott <seth.helgesen@gmail.com>
2023-02-16 08:02:49 -0500

    Update plesk-install.sh

--- plesk-install.sh
+++ plesk-install.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
 
-wget https://autoinstall.plesk.com/plesk-installer /tmp/plesk
-chmod +x /tmp/plesk-install/plesk-install.sh
-/tmp/plesk/plesk-installer --target /tmp/panel --select-release-latest --install-everything
+wget https://autoinstall.plesk.com/plesk-installer
+chmod +x /tmp/plesk-install/plesk-installer
+/tmp/plesk-install/plesk-installer --target /plesk/panel --select-release-latest --install-everything
